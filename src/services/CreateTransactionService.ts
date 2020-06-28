37ff9934dc2ffc375f6726444a4ab22cb67f0b2b
Alan Siqueira <alan.siqueira@outlook.com>
2020-06-28 15:34:24 -0300

    Challenge completed

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,20 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const currentBalance = this.transactionsRepository.getBalance();
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Tipo de transação inválido');
+    }
+
+    if (type === 'outcome' && value > currentBalance.total) {
+      throw Error('Saldo insuficiente para realizar essa operação');
+    }
+
+    const transaction = this.transactionsRepository.create({ title, value, type });
+
+    return transaction;
   }
 }
 
